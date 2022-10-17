97f57adb0a0a62add02ed1125588f9625ff8a1b2
Andre Loker <140714+aloker@users.noreply.github.com>
2022-10-17 20:29:04 +0200

    build: implement Clean task

--- build/Build.cs
+++ build/Build.cs
@@ -1,16 +1,9 @@
-using System;
-using System.Linq;
 using Nuke.Common;
-using Nuke.Common.CI;
-using Nuke.Common.Execution;
 using Nuke.Common.IO;
-using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.Npm;
 using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.Npm.NpmTasks;
 
 class Build : NukeBuild
@@ -29,6 +22,7 @@ class Build : NukeBuild
         .Before(Restore)
         .Executes(() =>
         {
+            RootDirectory.GlobDirectories("**/node_modules").ForEach(DeleteDirectory);
         });
 
     Target Restore => _ => _
